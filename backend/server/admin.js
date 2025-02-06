9e48c26c9e3647cb16432b432426a31fd8cffa60
Your Name <your-email@example.com>
2025-02-07 00:59:56 +0300

    add frontend folder

---
+++ backend/server/admin.js
@@ -0,0 +1,48 @@
+import express from "express"
+const route = express.Router();
+
+route.post("/api/addimage", async (req, res) => {
+  const {
+    body: {
+      data: { fullName, status, type, imageSrc },
+    },
+  } = req;
+ 
+  if (!fullName || !status || !type || !imageSrc)
+    return res.send({ message: "please fill out all the fields " });
+  try {
+    const exist = await imageModel.findOne({ fullName, type });
+    if (exist) return res.send({ message: `${fullName} already existed` });
+    const saveArtist = new imageModel({ fullName, status, type, imageSrc });
+    await saveArtist.save();
+    res.send({ message: "Artist saved succesfully" });
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
+route.delete("/api/delete/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    if (!id) return res.send({ message: `${fullName} has no id.` });
+    const artistToDel = await imageModel.findByIdAndDelete(id);
+    res.send({ message: await imageModel.find() });
+    console.log(artistToDel);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+route.patch("/api/put/:id", async (req, res) => {
+  const id = req.params.id;
+  console.log(req.body.updatedData);
+  try {
+    const artistToUpdate = await imageModel.findByIdAndUpdate(
+      id,
+      req.body.updatedData
+    );
+    res.send({ message: artistToUpdate });
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+export default route
---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import About from "./components/about/About"
+import Admin from "./components/admin/Admin"
+import Home from "./components/home/Home"
+import Login from "./components/loginSignup/Login"
+import Search from "./components/search/Search"
+
+
+function App() {
+  
+
+  return ( 
+    <>
+    <About />
+    
+      </>
+  )
+}
+
+export default App
