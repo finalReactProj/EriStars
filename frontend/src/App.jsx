f32205b12dd26976a23cafd0f3154b396706e6ad
Your Name <your-email@example.com>
2025-02-13 23:58:57 +0300

    add changes in app file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,27 @@
-import Contact from "./component/Contact/Contact";
+import About from "./component/about/About";
+//import Admin from "../src/component/admin/Admin";
+import Search from "./component/search/Search";
+import Home from "./component/Home/Home";
+import Header from "./component/header/Header";
+import Footer from "./component/Footer/Footer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+//import Login from "./component/login/Login";
 
 function App() {
   return (
     <>
-      <Contact />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+
+            <Route path="search" element={<Search />} />
+            <Route path="about" element={<About />} />
+      
+          {/* <Route path="/contact" element={<Contact/> } /> */}
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
