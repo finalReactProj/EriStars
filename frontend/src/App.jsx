1b93c7225f2449f36f6d8002a7ce145f494a57c0
luulsara <luulsara265@gmail.com>
2025-02-13 23:48:40 +0300

    crate login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import About from "./component/about/About"
-import Admin from "../src/component/admin/Admin"
+// import Admin from "../src/component/admin/Admin"
 import Search from "./component/search/Search"
 import Home from "./component/Home/Home"
 import Header from "./component/header/Header"
 import Footer from "./component/Footer/Footer"
 import {BrowserRouter,Route,Routes} from "react-router-dom"
+import Login from "./component/login/Login"
 
 
 function App() {
@@ -18,6 +19,9 @@ function App() {
           <Route path="/" element={<Home/> } />
           <Route path="/search" element={<Search/> } />
           <Route path="/about" element={<About/> } />
+          <Route path="/about" element={<About/> } />
+          <Route path="/login" element={<Login/> } />
+          
           {/* <Route path="/contact" element={<Contact/> } /> */}
         </Routes>
         <Footer/>
