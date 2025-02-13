d21466c56dbe3e076d0088a9fd5ba123b0cbcc9d
Your Name <your-email@example.com>
2025-02-14 00:35:22 +0300

    modify login folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,2 +1,32 @@
 
 
+import About from "./component/about/About";
+//import Admin from "../src/component/admin/Admin";
+import Search from "./component/search/Search";
+import Home from "./component/Home/Home";
+import Header from "./component/header/Header";
+import Footer from "./component/Footer/Footer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./component/login/Login";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={ <Login/>} />
+          {/* <Route path="/" element={<Home />} />
+
+          <Route path="search" element={<Search />} />
+          <Route path="about" element={<About />} /> */}
+
+          {/* <Route path="/contact" element={<Contact/> } /> */}
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
