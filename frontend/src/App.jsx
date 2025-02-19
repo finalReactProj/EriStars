3f671a479304b1e5bd0cafaab3dde1922c18a517
luulsara <luulsara265@gmail.com>
2025-02-19 18:17:41 +0300

    modify

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import About from "./component/about/About"
-import Admin from "../src/component/admin/Admin"
+// import Admin from "../src/component/admin/Admin"
 import Search from "./component/search/Search"
 import Home from "./component/Home/Home"
 import Header from "./component/header/Header"
 import Footer from "./component/Footer/Footer"
+import contact from "./component/contact"
 import {BrowserRouter,Route,Routes} from "react-router-dom"
 
 
@@ -18,7 +19,7 @@ function App() {
           <Route path="/" element={<Home/> } />
           <Route path="/search" element={<Search/> } />
           <Route path="/about" element={<About/> } />
-          {/* <Route path="/contact" element={<Contact/> } /> */}
+          <Route path="/contact" element={<Contact/> } />
         </Routes>
         <Footer/>
       </BrowserRouter>
